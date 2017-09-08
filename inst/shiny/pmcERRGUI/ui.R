d6885f6f7503f545ae998985d5ac83ef12c10925
IACCancu <hscancu@gmail.com>
2017-09-08 11:30:07 -0300

    Adding buttons to datatable in Downloads tab

--- inst/shiny/pmcERRGUI/ui.R
+++ inst/shiny/pmcERRGUI/ui.R
@@ -10,13 +10,16 @@ library(shiny)
 newSearchPage <- function() {
     wellPanel(
         fluidRow(
-            # column(6, selectInput(inputId='databaseInput', label='Database', list(PMC='pmc'), selected=NULL)),
             column(6, textInput(inputId='sepInput', label='Separator character', value='|'))),
         wellPanel(
             fluidRow(
-                column(5, fileInput(inputId='entitiesInputFile', label=NULL, buttonLabel='Load entities file')),
-                column(7, actionButton(inputId='searchbutton', label='Search', width='100%'))),
-            textAreaInput(inputId='entitiesInput', label='Entities', value='', height='600px', width='100%', resize='both', placeholder="In each line place a query, each entity must be | separated.\nEg:\nentity 1 | entity 2\nentity 3 | entity 4 | entity 5")
+                column(5, fileInput(inputId='entitiesInputFile', label=NULL,
+                                    buttonLabel='Load entities file')),
+                column(7, actionButton(inputId='searchbutton', label='Search',
+                                       width='100%'))),
+            textAreaInput(inputId='entitiesInput', label='Entities', value='',
+                          height='600px', width='100%', resize='both',
+                          placeholder="In each line place a query, each entity must be | separated.\nEg:\nentity 1 | entity 2\nentity 3 | entity 4 | entity 5")
         )
     )
 }
@@ -25,18 +28,22 @@ downloadPage <- function() {
     wellPanel(
         dataTableOutput(outputId='outputdtable'),
         fluidRow(
-            # column(4, numericInput(inputId='maxPapersInput', label=NULL, value=NA)),
-            column(4, textInput(inputId='maxPapersInput', label=NULL, placeholder='Max papers to download per entity')),
-            column(8, actionButton(inputId='downloadbutton', label='Download', width='100%')))
+            column(4, textInput(inputId='maxPapersInput', label=NULL,
+                            placeholder='Max papers to download per entity')),
+            column(8, actionButton(inputId='downloadbutton', label='Download',
+                                   width='100%')))
     )
 }
 
 relationsPage <- function() {
     wellPanel(
         fluidRow(
-            column(8, selectInput(inputId='entitySelInput', label='Entities', list(), selected=NULL, multiple=TRUE, size=10, selectize=FALSE)),
-            # column(1, actionButton(inputId='relsFilterbutton', label=NULL, icon=icon('filter'))),
-            column(4, selectInput(inputId='paperSelInput', label='Related papers IDs', list(), selected=NULL, size=10, selectize=FALSE))),
+            column(8, selectInput(inputId='entitySelInput', label='Entities',
+                                  list(), selected=NULL, multiple=TRUE, size=10,
+                                  selectize=FALSE)),
+            column(4, selectInput(inputId='paperSelInput',
+                                  label='Related papers IDs', list(),
+                                  selected=NULL, size=10, selectize=FALSE))),
         downloadButton('downloadResLink', 'Download results'),
         br(),
         br(),
@@ -52,7 +59,8 @@ relationsPage <- function() {
 }
 
 loadResPage <- function() {
-    fileInput(inputId='loadResInputFile', label=NULL, buttonLabel='Load results file', accept=c('.RData, .Rds, .rda'))
+    fileInput(inputId='loadResInputFile', label=NULL,
+              buttonLabel='Load results file', accept=c('.RData, .Rds, .rda'));
 }
 
 shinyUI(
